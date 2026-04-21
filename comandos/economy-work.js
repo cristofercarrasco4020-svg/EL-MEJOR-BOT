b0e0e1dd7f29b7c3663e14928d480f2501775f8b
Félix Ofx <xlfelixgamer@gmail.com>
2026-04-21 13:29:36 -0400

    Actualizar economy-work.js

--- comandos/economy-work.js
+++ comandos/economy-work.js
@@ -4,7 +4,6 @@ import path from 'path';
 import { workFrases } from './frases/work.js';
 
 const dbPath = path.resolve('./config/database/economy/economy.json');
-const workCooldowns = new Map();
 
 const workCommand = {
     name: 'work',
@@ -14,27 +13,31 @@ const workCommand = {
 
     run: async (conn, m) => {
         try {
-            const user = m.sender.split('@')[0];
+            const user = m.sender.split('@')[0].split(':')[0];
             const now = Date.now();
             const cooldown = 5 * 60 * 1000;
 
-            if (workCooldowns.has(user) && (now < workCooldowns.get(user) + cooldown)) {
-                const rem = workCooldowns.get(user) + cooldown - now;
-                return m.reply(`*${config.visuals.emoji2}* \`Descanso\`\n\nDebes esperar ${Math.floor(rem / 1000)}s para volver a chambear.\n\n> ¡El esfuerzo constante trae recompensas!`);
-            }
-
             let db = JSON.parse(fs.readFileSync(dbPath, 'utf-8'));
-            if (!db[user]) db[user] = { wallet: 0, bank: 0, daily: { lastClaim: 0, streak: 0 }, crime: { lastUsed: 0 } };
+            if (!db[user]) db[user] = { wallet: 0, bank: 0, work: { lastUsed: 0 } };
+            if (!db[user].work) db[user].work = { lastUsed: 0 };
+
+            const timePassed = now - db[user].work.lastUsed;
+
+            if (timePassed < cooldown) {
+                const rem = cooldown - timePassed;
+                return m.reply(`*${config.visuals.emoji2}* \`Descanso\`\n\nDebes esperar ${Math.floor(rem / 1000)}s para volver a chambear.`);
+            }
 
             const reward = Math.floor(Math.random() * (5000 - 2000 + 1)) + 2000;
             const frase = workFrases[Math.floor(Math.random() * workFrases.length)];
 
             db[user].wallet += reward;
-            fs.writeFileSync(dbPath, JSON.stringify(db, null, 2));
-            workCooldowns.set(user, now);
+            db[user].work.lastUsed = now; // Guardamos el tiempo en el JSON
+            
+            fs.writeFileSync(dbPath, JSON.stringify(db, null, 2), 'utf-8');
 
             await conn.sendMessage(m.chat, { 
-                text: `*${config.visuals.emoji3}* \`CHAMBA EXITOSA\`\n\n${frase}\n*${config.visuals.emoji} Ganaste:* ¥${reward.toLocaleString()}\n\n> ¡Tu trabajo ayuda a crecer el imperio!`
+                text: `*${config.visuals.emoji3}* \`CHAMBA EXITOSA\`\n\n${frase}\n*Ganaste:* ¥${reward.toLocaleString()}`
             }, { quoted: m });
 
         } catch (e) {
