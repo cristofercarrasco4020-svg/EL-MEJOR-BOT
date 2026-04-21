f996162c414ab8017264bc3cfef54c3d8c2601c8
Félix Ofx <xlfelixgamer@gmail.com>
2026-04-21 00:52:12 -0400

    Actualizar tools-tourl.js

--- comandos/tools-tourl.js
+++ comandos/tools-tourl.js
@@ -1,13 +1,6 @@
 import { config } from '../config.js';
 import { uploadToYotsuba } from '../config/UploadFile.js';
 
-function formatBytes(bytes) {
-    if (bytes === 0) return "0 B";
-    const sizes = ["B", "KB", "MB", "GB", "TB"];
-    const i = Math.floor(Math.log(bytes) / Math.log(1024));
-    return `${(bytes / 1024 ** i).toFixed(2)} ${sizes[i]}`;
-}
-
 const tourlCommand = {
     name: 'tourl',
     alias: ['url', 'imglink', 'subir'],
@@ -19,12 +12,12 @@ const tourlCommand = {
 
     run: async (conn, m, args, usedPrefix) => {
         const from = m.key.remoteJid;
-        
+
         const q = m.quoted ? m.quoted : m;
         const mime = (q.msg || q).mimetype || q.mediaType || '';
 
-        if (!mime || !/image|video|sticker|audio/.test(mime)) {
-            return m.reply(`*❁* \`Sin Multimedia\` *❁*\n\nNo detecto ninguna imagen o archivo.\n\n*✿︎* Responde a una imagen, sticker o video.\n*✿︎* O envía uno con el comando *${usedPrefix}tourl*`);
+        if (!mime || !/image|sticker/.test(mime)) {
+            return m.reply(`*❁* \`Sin Multimedia\` *❁*\n\nNo detecto ninguna imagen o sticker.\n\n*✿︎* Responde a una imagen o sticker estático.\n*✿︎* O envía uno con el comando *${usedPrefix}tourl*`);
         }
 
         try {
@@ -34,10 +27,9 @@ const tourlCommand = {
             if (!media) throw new Error('No se pudo descargar el medio.');
 
             const link = await uploadToYotsuba(media, mime);
-            const peso = formatBytes(media.length);
             const tipo = mime.split("/")[1].toUpperCase();
 
-            const textoExito = `*✿︎* \`Carga Exitosa\` *✿︎*\n\n*🚀 Enlace:* upload.yotsuba.giize.com${link}\n*⚖️ Peso:* ${peso}\n*📂 Tipo:* ${tipo}\n\n> Enlace generado para tu archivo multimedia.`;
+            const textoExito = `*✿︎* \`Carga Exitosa\` *✿︎*\n\n*🚀 Enlace:* upload.yotsuba.giize.com${link}\n*📂 Tipo:* ${tipo}\n\n> Enlace generado para tu archivo multimedia.`;
 
             await conn.sendMessage(from, { 
                 text: textoExito,
@@ -46,7 +38,7 @@ const tourlCommand = {
                         title: 'KAZUMA - TOURURL SERVICE',
                         body: `Archivo ${tipo} subido con éxito`,
                         thumbnailUrl: 'https://files.catbox.moe/9ssbf9.jpg', 
-                        sourceUrl: link,
+                        sourceUrl: `https://upload.yotsuba.giize.com${link}`,
                         mediaType: 1,
                         renderLargerThumbnail: false,
                         showAdAttribution: false
