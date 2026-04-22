3f6f1797cce63f18c3b024fe64152cac2770aa29
Félix Ofx <xlfelixgamer@gmail.com>
2026-04-22 15:27:56 -0400

    Actualizar main-ping.js

--- comandos/main-ping.js
+++ comandos/main-ping.js
@@ -11,19 +11,15 @@ const pingCommand = {
 
     run: async (conn, m) => {
         try {
-            // Marca de tiempo inicial
             const start = Date.now();
 
-            // Enviamos el mensaje primero para medir el tiempo de respuesta
             const pingMsg = await conn.sendMessage(m.chat, { 
                 text: `*${config.visuals.emoji2}* \`Verificando conexión...\`` 
             }, { quoted: m });
 
-            // Marca de tiempo final tras el envío
             const end = Date.now();
             const latencia = end - start;
 
-            // Editamos el mensaje con el diseño final y el ping real
             await conn.sendMessage(m.chat, { 
                 text: `*${config.visuals.emoji3}* \`KAZUMA PING\` *${config.visuals.emoji3}*\n\n*${config.visuals.emoji4} Velocidad:* ${latencia} ms\n*${config.visuals.emoji} Estado:* Online\n\n> *${config.visuals.emoji2}* \`SISTEMA OPERATIVO\``,
                 edit: pingMsg.key 
