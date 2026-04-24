02cc3da027819ff7d90960f90277abf31b2b2b75
Félix Ofx <xlfelixgamer@gmail.com>
2026-04-24 14:28:02 -0400

    Actualizar admins-testwelcome.js

--- comandos/admins-testwelcome.js
+++ comandos/admins-testwelcome.js
@@ -7,18 +7,22 @@ const testWelcome = {
     noPrefix: true,
 
     run: async (conn, m) => {
-        const user = m.sender;
-        let txt = `*${config.visuals.emoji3} \`WELCOME USER\` ${config.visuals.emoji3}*\n`;
-        txt += `› @${user.split('@')[0]}\n\n`;
-        txt += `» ¡Un nuevo alma se ha unido a nuestra travesía! Esperamos que tu estancia en este sector sea legendaria y llena de gloria.\n\n`;
-        txt += `> ${config.visuals.emoji} Usa el comando #help para ver mi lista de comandos.\n`;
-        txt += `> *${config.visuals.emoji4}* kazuma.giize.com`;
+        try {
+            const user = m.sender;
+            let txt = `*${config.visuals.emoji3} \`WELCOME USER\` ${config.visuals.emoji3}*\n`;
+            txt += `› @${user.split('@')[0]}\n\n`;
+            txt += `» ¡Un nuevo alma se ha unido a nuestra travesía! Esperamos que tu estancia en este sector sea legendaria y llena de gloria.\n\n`;
+            txt += `> ${config.visuals.emoji} Usa el comando #help para ver mi lista de comandos.\n`;
+            txt += `> *${config.visuals.emoji4}* kazuma.giize.com`;
 
-        await conn.sendMessage(m.chat, {
-            image: { url: config.img1 },
-            caption: txt,
-            mentions: [user]
-        }, { quoted: m });
+            await conn.sendMessage(m.chat, {
+                image: { url: config.img1 },
+                caption: txt,
+                mentions: [user]
+            }, { quoted: m });
+        } catch (e) {
+            m.reply(`*${config.visuals.emoji2}* Error al enviar la bienvenida. Verifica que la URL en config.img1 sea válida.`);
+        }
     }
 };
 
