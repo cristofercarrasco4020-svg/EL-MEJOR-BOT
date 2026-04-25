dde6c01b23b8b1625c804565d541c1135bf55758
TuNombre <tu@email.com>
2026-04-25 11:50:40 -0700

    Corregir gitignore: subir config.js y proteger carpeta config

---
+++ config.js
@@ -0,0 +1,30 @@
+import fs from 'fs';
+import path from 'path';
+
+export const config = {
+    botName: 'Kazuma',
+    owner: [
+        '5216653470605@s.whatsapp.net', 
+        '125860308893859@lid'
+    ], 
+    prefix: '#',
+    allPrefixes: ['#', '!', '.'],
+
+    getBotType: (conn) => {
+        const userNumber = conn.user.id.split(':')[0];
+        const subBotPath = path.resolve(`./sesiones_subbots/${userNumber}`);
+        return fs.existsSync(subBotPath) ? '*Sub-Bot*' : '*Mood*';
+    },
+
+    visuals: {
+        line: '━',
+        color: 'magenta',
+        emoji: '✰',
+        emoji2: '❁',
+        emoji3: '✿',
+        emoji4: '❀',
+        img1: 'https://files.catbox.moe/9ssbf9.jpg'
+    },
+
+    apiNex: 'NEX-0868C926ADF94B19A51E18C4'
+};
